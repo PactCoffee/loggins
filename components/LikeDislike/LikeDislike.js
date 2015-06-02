fe01a1777d7cdb36e48c4af77ec4be82ee05eb6d
Iestyn Williams <iest@me.com>
2015-06-02 17:50:19 +0100

    LikeDislike button first pass

---
+++ components/LikeDislike/LikeDislike.js
@@ -0,0 +1,80 @@
+import React, {Component, PropTypes} from 'react';
+
+import styles from './LikeDislike.css';
+import * as m from 'globals/modifiers.css';
+import Icon from 'components/Icon/Icon';
+
+export default class LikeDislike extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: props.liked === true,
+      disliked: props.liked === false
+    };
+    this.like = this.like.bind(this);
+    this.dislike = this.dislike.bind(this);
+    this.id = new Date().getTime();
+  }
+
+  like() {
+    this.setState({
+      liked: true,
+      disliked: false
+    });
+    this.props.onChange(true);
+  }
+
+  dislike() {
+    this.setState({
+      liked: false,
+      disliked: true
+    });
+    this.props.onChange(false);
+  }
+
+  render() {
+    const outerStyles = [
+      styles.root,
+      this.state.liked ? styles.liked:null,
+      this.state.disliked ? styles.disliked:null
+    ].join(' ');
+
+    return (
+      <form className={outerStyles}>
+
+        <input
+          id={`like-${this.id}`}
+          className={styles.like}
+          onChange={this.like}
+          checked={this.state.liked}
+          type="radio"
+        />
+        <label htmlFor={`like-${this.id}`}>
+          <Icon name="thumb"/>
+        </label>
+
+        <input
+          id={`dislike-${this.id}`}
+          className={styles.dislike}
+          onChange={this.dislike}
+          checked={this.state.disliked}
+          type="radio"
+        />
+        <label htmlFor={`dislike-${this.id}`}>
+          <Icon className={m.rotate180} name="thumb"/>
+        </label>
+      </form>
+    );
+  }
+}
+
+LikeDislike.propTypes = {
+
+  // true = liked
+  // false = disliked
+  // null = no selection
+  liked: PropTypes.any,
+
+  // Called with the user's preference
+  onChange: PropTypes.func.isRequired
+};
--- styleguide/sections/Buttons.js
+++ styleguide/sections/Buttons.js
@@ -2,20 +2,48 @@ import React, {Component} from 'react';
 
 import Section from './Section';
 import Btn from 'components/Btn/Btn';
+import Icon from 'components/Icon/Icon';
+import LikeDislike from 'components/LikeDislike/LikeDislike';
+
 
 export default class BtnSection extends Component {
   constructor(props) {
     super(props);
-    this.linkName = 'Buttons';
+    this.state = {
+      liked: null
+    };
+    this.likeChanged = this.likeChanged.bind(this);
   }
+
+  likeChanged(didLike) {
+    this.setState({liked: didLike});
+  }
+
   render() {
     return (
-      <Section name={this.linkName}>
+      <Section name="Buttons" href="https://github.com/PactCoffee/loggins/blob/master/styleguide%2Fsections%2FButtons.js">
 
         <Btn primary>Primary</Btn>&nbsp;
         <Btn secondary>Secondary</Btn>&nbsp;
         <Btn warning>Warning</Btn>&nbsp;
-        <Btn>Standard</Btn>
+        <Btn>Standard</Btn>&nbsp;
+
+        <Btn>
+          <Icon name="heart"/>
+          &nbsp;
+          Avec icon
+          &nbsp;
+          <Icon name="coffee"/>
+        </Btn>
+
+        <br/>
+        <br/>
+
+        <LikeDislike onChange={this.likeChanged}/>
+        &nbsp;
+        {this.state.liked === true ? 'Liked': null}
+        {this.state.liked === false ? 'Disliked': null}
+        {this.state.liked === null ? 'No preference': null}
 
       </Section>
     );
