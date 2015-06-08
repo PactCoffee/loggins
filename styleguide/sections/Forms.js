5332d2e9cdc173b3d0688c8cbe84ee0edc8f427a
Iestyn Williams <iest@me.com>
2015-06-08 14:35:06 +0100

    SelectableInput improvements, add to styleguide

--- styleguide/sections/Forms.js
+++ styleguide/sections/Forms.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import Section from './Section';
 import EmailMultiInput from 'components/EmailMultiInput/EmailMultiInput';
+import SelectableInput from 'components/SelectableInput/SelectableInput';
 
 export default class FormSection extends Component {
 
@@ -17,11 +18,15 @@ export default class FormSection extends Component {
 
   render() {
     return (
-      <Section name="Forms">
+      <Section name="Forms" href="https://github.com/PactCoffee/loggins/blob/master/styleguide/sections/Forms.js">
 
-        <p>An EmailMultiInput:</p>
-
+        <h3>EmailMultiInput</h3>
+        <p>Enter multiple email addresses with a tagging interface.</p>
         <EmailMultiInput emails={this.state.emails} placeholder="Emails" onChange={this.emailsChanged} />
+
+        <h3>SelectableInput</h3>
+        <p>A read-only textarea who's contents are selected on click. It's size is dictated by it's contents using a hidden div.</p>
+        <SelectableInput value="this text will get highlighted on click, making it fast to copy"/>
       </Section>
     );
   }
