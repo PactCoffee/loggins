77b9111c24e01b07689b17a904f7e1ee8e3a867f
Iestyn Williams <iest@me.com>
2015-06-09 10:08:40 +0100

    FormInput component

---
+++ components/FormInput/FormInput.js
@@ -0,0 +1,67 @@
+import React, {Component, PropTypes} from 'react';
+
+import css from './FormInput.css';
+
+export default class FormInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+
+    this.state = {
+      active: !!props.value
+    };
+  }
+
+  handleFocus() {
+    this.setState({active: true});
+  }
+  handleBlur() {
+    this.setState({active: !!this.props.value});
+  }
+
+  render() {
+
+    const outerCSS = [
+      css.container,
+      this.props.error ? css.containerError:null,
+      this.state.active ? css.containerActive:null
+    ].join(' ');
+
+    const messageCSS = [
+      css.message,
+      this.props.error ? css.messageError:null
+    ].join(' ');
+
+    return (
+      <div className={outerCSS}>
+        <label className={css.label}>
+          {this.props.label}
+        </label>
+        <input
+          className={css.input}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          onChange={this.props.onChange}
+          defaultValue={this.props.value}
+        />
+        <span className={messageCSS}>
+          {this.props.error}
+        </span>
+      </div>
+    );
+  }
+}
+
+FormInput.propTypes = {
+
+  value: PropTypes.string.isRequired,
+
+  onChange: PropTypes.func.isRequired,
+
+  label: PropTypes.string.isRequired,
+
+  // If set, will display underneath the input
+  error: PropTypes.string
+};
--- styleguide/sections/Forms.js
+++ styleguide/sections/Forms.js
@@ -3,20 +3,31 @@ import React, {Component} from 'react';
 import Section from './Section';
 import EmailMultiInput from 'components/EmailMultiInput/EmailMultiInput';
 import SelectableInput from 'components/SelectableInput/SelectableInput';
+import FormInput from 'components/FormInput/FormInput';
 
 export default class FormSection extends Component {
 
   constructor(props) {
     super(props);
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleCopy = this.handleCopy.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+
     this.state = {
       emails: [],
-      value: 'highlight me by clicking'
+      text: ''
     };
-    this.emailsChanged = this.emailsChanged.bind(this);
-    this.handleCopy = this.handleCopy.bind(this);
   }
 
-  emailsChanged(emails) {
+  handleTextChange(e) {
+    this.setState({
+      text: e.target.value,
+      error: e.target.value > 5 ? 'Yep, you fucked up':''
+    });
+  }
+
+  handleEmailChange(emails) {
     this.setState({emails: emails});
   }
 
@@ -30,11 +41,14 @@ export default class FormSection extends Component {
 
         <h3>EmailMultiInput</h3>
         <p>Enter multiple email addresses with a tagging interface.</p>
-        <EmailMultiInput emails={this.state.emails} placeholder="Emails" onChange={this.emailsChanged} />
+        <EmailMultiInput emails={this.state.emails} placeholder="Emails" onChange={this.handleEmailChange} />
 
         <h3>SelectableInput</h3>
         <p>A read-only textarea who's contents are selected on click. It's size is dictated by it's contents using a hidden div. Will fire it's <code>onAction</code> prop when the user copies any of the text within the textarea.</p>
-        <SelectableInput value={this.state.value} onAction={this.handleCopy}/>
+        <SelectableInput value="Highlight me by clicking. If you copy text, you'll be alerted" onAction={this.handleCopy}/>
+
+        <FormInput onChange={this.handleTextChange} value={this.state.text} error={this.state.error} label="First name"/>
+
       </Section>
     );
   }
