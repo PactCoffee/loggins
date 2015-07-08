4e1b100e2ba649e82d5bd19eb6f2eb5df9da332a
Iestyn Williams <iest@me.com>
2015-07-08 10:00:15 +0100

    Add Wrapper component to set max-width in container

---
+++ components/Wrapper/Wrapper.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import styles from './Wrapper.css';
+
+export default class Wrap extends Component {
+  render() {
+    return (
+      <div className={[styles.root, this.props.className].join(' ')}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Wrap.propTypes = {
+  children: React.PropTypes.any,
+  className: React.PropTypes.string
+};
