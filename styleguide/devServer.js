7fbf23d3569563524f9defa413b9e59da123b04a
Iestyn Williams <iest@me.com>
2015-07-10 15:27:54 +0100

    Change dev server port so less confusing

--- styleguide/devServer.js
+++ styleguide/devServer.js
@@ -6,10 +6,10 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(8181, 'localhost', function(err) {
+}).listen(8080, 'localhost', function(err) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:8181');
+  console.log('Listening at localhost:8080');
 });
