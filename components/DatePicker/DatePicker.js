6496ddc72b794f6118aa2f8283b6832b9c11a141
Iestyn Williams <iest@me.com>
2015-07-13 19:28:35 +0100

    Prettier WIP

--- components/DatePicker/DatePicker.js
+++ components/DatePicker/DatePicker.js
@@ -3,16 +3,33 @@
 import React, {Component, PropTypes} from 'react';
 import {isSameDay} from 'react-day-picker/lib/Utils';
 import DayPicker from 'react-day-picker';
-import {LocaleUtils} from 'react-day-picker/lib/addons';
 import moment from 'moment';
 
 import s from './DatePicker.css';
+
+const weekdaysShort = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const weekdaysLong = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+];
+
+const localeUtils = {
+  formatMonthTitle: date => moment(date).format('MMMM YYYY'),
+  formatWeekdayShort: dayIndex => weekdaysShort[dayIndex],
+  formatWeekdayLong: dayIndex => weekdaysLong[dayIndex],
+  getFirstDayOfWeek: () => 1
+}
 
 function cantShip(day) {
   const now = moment();
   const then = moment(day);
   const lastShippable = moment(now).set({
-    hour: 15,
+    hour: 13,
     minute: 0,
     second: 0,
     millisecond: 0
@@ -36,6 +53,12 @@ export default class DatePicker extends Component {
   constructor(props) {
     super(props);
     this.handleDayClick = this.handleDayClick.bind(this);
+    this.renderDay = this.renderDay.bind(this);
+  }
+  renderDay(day) {
+    return (
+      <span>{day.getDate()}</span>
+    );
   }
   render() {
     const selectedDay = new Date(this.props.value);
@@ -47,9 +70,18 @@ export default class DatePicker extends Component {
     return (
       <div className={s.root}>
         <DayPicker locale="en-gb"
-                   localeUtils={LocaleUtils}
+                   enableOutsideDays={true}
+                   localeUtils={localeUtils}
                    modifiers={modifiers}
+                   renderDay={this.renderDay}
                    onDayClick={this.handleDayClick}/>
+        <p>Ships {moment(this.props.value).set({
+            hour: 19,
+            minute: 0,
+            second: 0,
+            millisecond: 0
+          }).fromNow()}
+        </p>
       </div>
     );
   }
