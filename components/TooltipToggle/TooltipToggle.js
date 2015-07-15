806c89670b390f8a1b91aaacd5cdee67207db9bf
Iestyn Williams <iest@me.com>
2015-07-15 14:23:20 +0100

    Better TooltipToggle API & example

---
+++ components/TooltipToggle/TooltipToggle.js
@@ -0,0 +1,66 @@
+import React, {Component, cloneElement, PropTypes} from 'react';
+
+import HoverCard from '../HoverCard/HoverCard';
+
+export default class TooltipToggle extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleHover = this.handleHover.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleHover() {
+    this.setState({
+      show: true
+    });
+  }
+  handleBlur() {
+    this.setState({
+      show: false
+    });
+  }
+
+  render() {
+    const {container, content, placement, children} = this.props;
+    const trigger = cloneElement(React.Children.only(children), {
+      onMouseOver: this.handleHover,
+      onFoucus: this.handleHover,
+      onMouseOut: this.handleBlur,
+      onBlur: this.handleBlur,
+      ref: 'anchor'
+    });
+    return (
+      <span>
+        {trigger}
+        {this.state.show ?
+          <HoverCard container={container}
+                     variant="tooltip"
+                     anchor={this.refs.anchor}
+                     anchorPading={10}
+                     placement={placement}
+                     caret={true}>
+            {content}
+          </HoverCard>
+          : null
+        }
+      </span>
+    );
+  }
+}
+
+TooltipToggle.propTypes = {
+  content: PropTypes.node.isRequired,
+  children: PropTypes.element.isRequired,
+  container: PropTypes.element.isRequired,
+  placement: PropTypes.oneOf(['top', 'left', 'bottom', 'right'])
+};
+
+TooltipToggle.defaultProps = {
+  placement: 'top'
+};
+
--- styleguide/sections/Overlays.js
+++ styleguide/sections/Overlays.js
@@ -1,99 +1,21 @@
 import React, {Component} from 'react';
 
 import Section from '../components/Section';
-import * as m from 'globals/modifiers.css';
 
-import HoverCard from 'components/HoverCard/HoverCard';
+import TooltipToggle from 'components/TooltipToggle/TooltipToggle';
 
 export default class Overlays extends Component {
-  constructor(props) {
-    super(props);
-    this.handleHover = this.handleHover.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-
-    this.state = {
-      showCard: false
-    };
-  }
-
-  componentDidMount() {
-    this.handleHover();
-  }
-
-  handleHover() {
-    this.setState({
-      showCard: true
-    });
-  }
-  handleBlur() {
-    this.setState({
-      // showCard: false
-    });
-  }
-
   render() {
+
+    const tooltip = <TooltipToggle container={this} content="Hello from inside a tooltip!"><strong>these words</strong></TooltipToggle>;
+
     return (
       <Section name="Overlays" href="https://github.com/PactCoffee/loggins/blob/master/styleguide/sections/Overlays.js">
 
-        <h3>HoverCard</h3>
-        <div className={m.alignc}>
-          <strong ref="anchor"
-             onMouseOver={this.handleHover}
-             onMouseLeave={this.handleBlur}
-             variant="tooltip">
-            WWWWWOWWWWW
-          </strong>
-        </div>
-        {this.state.showCard ?
-          <span>
-            <HoverCard container={this}
-                       anchor={this.refs.anchor}
-                       placement="left"
-                       variant="tooltip">
-              left
-            </HoverCard>
-            <HoverCard container={this}
-                       anchor={this.refs.anchor}
-                       placement="top"
-                       variant="tooltip">
-              Top placement tooltip
-            </HoverCard>
-            <HoverCard container={this}
-                       anchor={this.refs.anchor}
-                       placement="right"
-                       variant="tooltip">
-              right
-            </HoverCard>
-            <HoverCard container={this}
-                       anchor={this.refs.anchor}
-                       placement="bottom"
-                       variant="tooltip">
-              bottom
-            </HoverCard>
-          </span>
-          : null
-        }
-
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+        <h3>TooltipToggle</h3>
+        <p>
+          Try hovering over {tooltip}. The child passed into TooltipToggle must be a react element. For this example, a strong is passed in.
+        </p>
 
       </Section>
     );
