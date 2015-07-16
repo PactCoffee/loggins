d65296d3c7e706bd5a96148dcfc7f138eb1f556f
Iestyn Williams <iest@me.com>
2015-07-16 09:23:39 +0100

    Extra Dropdown examples

--- styleguide/sections/Overlays.js
+++ styleguide/sections/Overlays.js
@@ -72,10 +72,35 @@ export default class Overlays extends Component {
         </p>
         <div style={{textAlign: 'center'}}>
           <Dropdown container={this} trigger={
-            <Btn>Toggle dropdown <Icon name="triangle"/></Btn>
+            <Btn className={m.mbs}>Toggle dropdown <Icon name="triangle"/></Btn>
           }>
             Some content inside the dropdown
           </Dropdown>
+
+          <br/>
+
+          <Dropdown placement="left" container={this} trigger={
+            <Btn className={m.mbs}>Toggle left <Icon className={m.rotate90} name="triangle"/></Btn>
+          }>
+            Left!
+          </Dropdown>
+
+          <br/>
+
+          <Dropdown placement="right" container={this} trigger={
+            <Btn className={m.mbs}>Toggle right <Icon className={m.rotate270} name="triangle"/></Btn>
+          }>
+            Right!
+          </Dropdown>
+
+          <br/>
+
+          <Dropdown placement="top" container={this} trigger={
+            <Btn className={m.mbs}>Toggle top <Icon className={m.rotate180} name="triangle"/></Btn>
+          }>
+            Top!
+          </Dropdown>
+
         </div>
 
       </Section>
