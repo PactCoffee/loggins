37ad4abc88d51c35b35ce5152f5b3fe10ed3b2a6
Iestyn Williams <iest@me.com>
2015-07-23 13:48:21 +0100

    Improve Logo, fix sizing issue

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,23 +1,8 @@
 import React, {PropTypes} from 'react';
-import invariant from 'react/lib/invariant';
 
 import styles from './Logo.css';
 
 export default class Logo {
-  constructor(props) {
-    invariant(
-      props.size === 'large' ||
-      props.size === 'medium' ||
-      props.size === 'small',
-      `Logo(...): You must supply a valid size: large/medium/small`
-    );
-    invariant(
-      props.type === 'horizontal' ||
-      props.type === 'vertical',
-      `Logo(...): You must supply a valid type: horizontal/vertical`
-    );
-  }
-
   render() {
     const {type, size} = this.props;
     const cssClasses = [
@@ -35,7 +20,7 @@ export default class Logo {
 }
 
 Logo.propTypes = {
-  size: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']).isRequired,
+  type: PropTypes.oneOf(['horizontal', 'vertical']).isRequired,
   className: PropTypes.string
 };
