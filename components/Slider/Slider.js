698c2bf92a1b782631db427a9fb07ba4bb3bf2e9
Iestyn Williams <iest@me.com>
2015-08-03 14:57:07 +0100

    Add ticks option to slider to show values underneath

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -4,15 +4,45 @@ import ReactSlider from 'react-slider';
 
 import styles from './Slider.css';
 
+const BAR_CLASS_NAME = 'SliderBar';
+
 export default class Slider {
+  constructor() {
+    this.generateTicks = this.generateTicks.bind(this);
+  }
+  generateTicks() {
+    const {ticks, min, max} = this.props;
+    const tickStep = (max - min) / ticks;
+    const allTicks = Array
+      .apply(null, {length: ticks + 1})
+      .map((u, i) => Math.floor(min + (tickStep * i)));
+    return allTicks;
+  }
   render() {
+    const {ticks} = this.props;
     return (
-      <ReactSlider withBars
-                   className={styles.slider}
-                   barClassName={styles.bar}
-                   handleClassName={styles.handle}
-                   handleActiveClassName={styles.activeHandle}
-                   {...this.props}/>
+      <div>
+        <ReactSlider withBars
+                     className={styles.slider}
+                     barClassName={BAR_CLASS_NAME}
+                     handleClassName={styles.handle}
+                     handleActiveClassName={styles.activeHandle}
+                     {...this.props}/>
+        {ticks ?
+          <div className={styles.tickContainer}>
+            <span className={styles.ticks}>
+              {this.generateTicks().map((tick, i, arr) =>
+                <span key={i}
+                      className={styles.tick}
+                      style={{left: `${(100 / (arr.length - 1)) * i}%`}}>
+                  {tick}
+                </span>
+              )}
+            </span>
+          </div>
+          : null
+        }
+      </div>
     );
   }
 }
@@ -20,6 +50,12 @@ export default class Slider {
 Slider.propTypes = {
   onChange: PropTypes.func.isRequired,
   value: PropTypes.number.isRequired,
+  ticks: PropTypes.number,
   min: PropTypes.number,
   max: PropTypes.number
+};
+
+Slider.defaultProps = {
+  min: 0,
+  max: 100
 };
