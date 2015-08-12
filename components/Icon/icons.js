004e5906c206138da31356aca91e6abcafe10479
Iestyn Williams <iest@me.com>
2015-08-12 12:24:56 +0100

    New icons, improve some icons

--- components/Icon/icons.js
+++ components/Icon/icons.js
@@ -28,11 +28,13 @@ export default {
 
   cog: cleanupSvg(require('./icons/cog.svg')),
   pin: cleanupSvg(require('./icons/pin.svg')),
+  bean: cleanupSvg(require('./icons/bean.svg')),
   tick: cleanupSvg(require('./icons/tick.svg')),
   more: cleanupSvg(require('./icons/more.svg')),
   clock: cleanupSvg(require('./icons/clock.svg')),
   heart: cleanupSvg(require('./icons/heart.svg')),
   thumb: cleanupSvg(require('./icons/thumb.svg')),
+  cross: cleanupSvg(require('./icons/cross.svg')),
   coffee: cleanupSvg(require('./icons/coffee.svg')),
   chevron: cleanupSvg(require('./icons/chevron.svg')),
   twitter: cleanupSvg(require('./icons/twitter.svg')),
