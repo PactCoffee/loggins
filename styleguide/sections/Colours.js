7d6d3f49c7c487fe668fa4e3c2f9bb5580c7330f
Iestyn Williams <iest@me.com>
2015-08-25 15:42:58 +0100

    Add all our colours to styleguide

--- styleguide/sections/Colours.js
+++ styleguide/sections/Colours.js
@@ -1,33 +1,79 @@
+/*eslint react/no-multi-comp:0*/
 import React, {Component} from 'react';
 import Section from '../components/Section';
 
 import styles from '../styleguide.css';
 import * as t from 'globals/typography.css';
 
+class Swatch {
+  render() {
+    const {i, colour} = this.props;
+    return (
+      <div key={i} className={[styles.swatch, styles[colour], t.title].join(' ')}>
+        <span>{colour}</span>
+      </div>
+    );
+  }
+}
+Swatch.propTypes = {
+  color: React.PropTypes.string.isRequired,
+  i: React.PropTypes.number.isRequired
+};
+
 export default class ColourSection extends Component {
   render() {
-
-    const colours = [
-      'green',
-      'red',
-      'orange',
-      'grey',
-      'greyLight',
-      'brown',
-      'brownDark',
-      'white',
-      'whiteDark'
-    ];
-
     return (
       <Section name="Colours">
-        {
-          colours.map((colour, i) =>
-            <div key={i} className={[styles.swatch, styles[colour], t.title].join(' ')}>
-              <span>{colour}</span>
-            </div>
-          )
+        <p>The main palette we use:</p>
+        {['primary',
+          'secondary',
+          'error',
+          'alert',
+          'text',
+          'muted'].map((c, i) => <Swatch key={i} colour={c}/>)
         }
+
+        <p>9 shades of grey:</p>
+        {['white',
+          'offwhite',
+          'white-dark',
+          'grey-lightest',
+          'grey-lighter',
+          'grey-light',
+          'grey',
+          'black'].map((c, i) => <Swatch key={i} colour={c}/>)
+        }
+
+        <p>Even more colours</p>
+        {['green',
+          'red',
+          'orange',
+          'brown',
+          'brown-dark'].map((c, i) => <Swatch key={i} colour={c}/>)
+        }
+
+        <p>Various blues:</p>
+
+        {['blue-muted',
+          'blue-harsh',
+          'blue',
+          'blue-dark',
+          'blue-darker',
+          'steel'].map((c, i) => <Swatch key={i} colour={c}/>)
+        }
+
+        <p>Official™ Pact colour scheme:</p>
+        {['teal',
+          'maroon',
+          'taupe',
+          'yellow',
+          'mauve',
+          'copper',
+          'pact'].map((c, i) => <Swatch key={i} colour={c}/>)
+        }
+
+        <p>Third parties:</p>
+        {['twitter', 'facebook'].map((c, i) => <Swatch key={i} colour={c}/>)}
       </Section>
     );
   }
