3c06c88061ab20c3f90a39b649e6e5dfba168fb5
Rich Palmer <rich@rdjpalmer.com>
2015-10-16 14:32:33 +0100

    MaskedFormInput with payment examples

---
+++ components/MaskedFormInput/MaskedFormInput.js
@@ -0,0 +1,99 @@
+import React, {Component, PropTypes} from 'react';
+import {uniqueId} from 'lodash';
+
+import MaskedInput from 'react-maskedinput';
+
+import css from '../FormInput/FormInput.css';
+
+export default class MaskedFormInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+
+    this.state = {
+      focus: false,
+      id: uniqueId('forminput'),
+    };
+  }
+
+  handleFocus() {
+    this.setState({
+      focus: true,
+    });
+  }
+
+  handleBlur() {
+    this.setState({
+      focus: false,
+    });
+  }
+
+  render() {
+    const {
+      placeholder,
+      borderless,
+      onChange,
+      error,
+      value,
+      label,
+      type,
+      pattern,
+    } = this.props;
+    const {focus, id} = this.state;
+
+    const active = value && !!value.length;
+
+    const outerCSS = [
+      css.container,
+      placeholder ? css.labelInside : css.labelOutside,
+      error ? css.containerError : null,
+      active ? css.containerActive : null,
+      focus ? css.containerFocus : null,
+      borderless ? css.borderless : null,
+    ].join(' ');
+
+    const messageCSS = [
+      css.message,
+      error ? css.messageError : null,
+    ].join(' ');
+
+    return (
+      <div className={outerCSS}>
+        <label htmlFor={id} className={css.label}>
+          {label}
+        </label>
+        <MaskedInput
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          className={css.input}
+          onChange={onChange}
+          value={value}
+          id={id}
+          type={type}
+          pattern={pattern}
+          placeholder={placeholder}
+        />
+        <span className={messageCSS}>
+          {this.props.error}
+        </span>
+      </div>
+    );
+  }
+}
+
+MaskedFormInput.propTypes = {
+
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  pattern: PropTypes.string.isRequired,
+
+  placeholder: PropTypes.string,
+  borderless: PropTypes.bool,
+
+  // If set, will display underneath the input
+  error: PropTypes.string,
+};
