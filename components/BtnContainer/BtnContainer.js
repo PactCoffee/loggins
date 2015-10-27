4dd2b2960a0a83d446f80820da870ecffd0dfce4
Rich Palmer <rich@rdjpalmer.com>
2015-10-27 10:22:28 +0000

    BtnContainer component

---
+++ components/BtnContainer/BtnContainer.js
@@ -0,0 +1,20 @@
+import React, {PropTypes} from 'react';
+
+import css from './BtnContainer.css';
+
+export default class BtnContainer {
+  render() {
+    const {children, className, ...props} = this.props;
+
+    return (
+      <button {...props} className={[css.root, className].join(' ')}>
+        {children}
+      </button>
+    );
+  }
+}
+
+BtnContainer.propTypes = {
+  children: PropTypes.any.isRequired,
+  className: PropTypes.string,
+};
