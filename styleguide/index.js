fb6c173105ee685360688edcbac85a649f8797fa
Iestyn Williams <iest@me.com>
2015-12-27 10:13:07 -0500

    Update to react & react-dom 0.14

--- styleguide/index.js
+++ styleguide/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { Router } from 'react-router';
 import { createHashHistory } from 'history';
 
 import routes from './routes';
 
-React.render(
+render(
   <Router
     history={createHashHistory()}
     children={routes}
   />,
-  document.getElementById('outlet'));
+  document.getElementById('outlet')
+);
