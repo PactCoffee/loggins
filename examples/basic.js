290f6f20ebc539101ee3c8af47c0c9b28cfed64c
Iestyn Williams <iest@me.com>
2015-12-27 16:51:07 -0500

    Kill examples (useless)

--- examples/basic.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-import Btn from '../components/Btn/Btn';
-
-export class Basic extends Component {
-  render() {
-    return <Btn>Basic example</Btn>
-  }
-}
