0e10bb0df0726a33526f83b64cca3c53de9fdae5
Adrian Pacała <altpacala@gmail.com>
2017-05-23 09:17:02 +0100

    Export key codes as constants

--- util/keyCodes.js
+++ util/keyCodes.js
@@ -1,8 +1,7 @@
-export default {
-  backspace: 8,
-  tab: 9,
-  enter: 13,
-  del: 46,
-  comma: 188,
-  esc: 27,
-};
+export const BACKSPACE = 8;
+export const TAB = 9;
+export const SPACE = 32;
+export const ENTER = 13;
+export const DEL = 46;
+export const COMMA = 188;
+export const ESC = 27;
